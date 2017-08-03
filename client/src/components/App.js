4062eeb6ac94e555d5890b31dfe8a83d35a3586f
Frederico Teixeira <mrfrederico89@gmail.com>
2017-08-03 09:41:20 +0100

    add Login component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,50 +1,12 @@
 import React, { Component } from 'react'
-import styled, { keyframes } from 'styled-components'
 
-import logo from '../res/logo.svg'
+import Login from './Login'
 
 class App extends Component {
   render() {
-    return (
-      <Container>
-        <Header>
-          <Logo src={logo} alt="logo" />
-          <h2>Welcome to React</h2>
-        </Header>
-        <Intro>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </Intro>
-        <a href="http://localhost:4000/login">Login with GitHub</a>
-      </Container>
-    )
+    return <Login />
   }
 }
-
-// styles ===========
-const Container = styled.div`text-align: center;`
-
-const Header = styled.div`
-  background-color: #222;
-  height: 150px;
-  padding: 20px;
-  color: white;
-`
-
-const logoSpin = keyframes`
-from {
-  transform: rotate(0deg);
-}
-to {
-  transform: rotate(360deg);
-}
-`
-
-const Logo = styled.img`
-  animation: ${logoSpin} infinite 20s linear;
-  height: 80px;
-`
-
-const Intro = styled.p`font-size: large;`
 
 // ==================
 export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { injectGlobal } from 'styled-components'
 
 import App from './components/App'
 import registerServiceWorker from './utils/registerServiceWorker'
-
-// eslint-disable-next-line no-unused-expressions
-injectGlobal`
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: sans-serif;
-  }
-`
 
 // ====================
 ReactDOM.render(<App />, document.getElementById('root'))
