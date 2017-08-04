5b093ffb854c7ec74ebb2f752200f39c707b63f7
Frederico Teixeira <mrfrederico89@gmail.com>
2017-08-04 10:36:49 +0100

    create Story and Comment graphQL schema

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,12 +1,56 @@
-const userHandler = (rootValue, args, { user }) => {
+const axios = require('axios')
+
+// handlers =======
+const user = (rootValue, args, { user }) => {
   if (user) return user
   return null
 }
 
+const topStories = async (root, { first, after }) => {
+  const res = await axios.get(
+    'https://hacker-news.firebaseio.com/v0/topstories.json',
+  )
+
+  return res.data.slice(after, after + first).map(async storyId => {
+    const res = await axios.get(
+      `https://hacker-news.firebaseio.com/v0/item/${storyId}.json`,
+    )
+
+    const { id, title, url, descendants, kids, time } = res.data
+    return {
+      id,
+      title,
+      url,
+      numComments: descendants,
+      commentsIds: kids,
+      creationDate: time,
+    }
+  })
+}
+
+const storyComments = async (root, { storyId }) => {
+  const res = await axios.get(
+    `https://hacker-news.firebaseio.com/v0/item/${storyId}.json`,
+  )
+
+  return res.data.kids.map(async commentId => {
+    const res = await axios.get(
+      `https://hacker-news.firebaseio.com/v0/item/${commentId}.json`,
+    )
+
+    const { id, by, text, time } = res.data
+    return {
+      id,
+      author: by,
+      text,
+      creationDate: time,
+    }
+  })
+}
+
+// ================
 const resolverMap = {
-  Query: {
-    user: userHandler,
-  },
+  Query: { user, topStories, storyComments },
 }
 
 // ================
--- server/src/schema.js
+++ server/src/schema.js
@@ -5,12 +5,29 @@ const resolvers = require('./resolvers')
 const typeDefs = `
   type User {
     id: ID!
-    githubId: ID!
+    githubId: ID
     token: String
+  }
+
+  type Story {
+    id: ID!
+    title: String
+    url: String
+    numComments: Int
+    creationDate: Int
+  }
+
+  type Comment {
+    id: ID!
+    author: String
+    text: String
+    creationDate: Int
   }
 
   type Query {
     user: User
+    topStories(first: Int, after: Int): [Story]
+    storyComments(storyId: Int): [Comment]
   }
 `
 
